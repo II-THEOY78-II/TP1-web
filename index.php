ad34a833342b09719566a0e27cf119cdfa56c80c
Yanom <65492446+II-THEOY78-II@users.noreply.github.com>
2023-09-11 20:20:04 +0200

    Projet fil rouge

--- index.php
+++ index.php
@@ -2,35 +2,34 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Mon site de recettes</title>
+        <title>Mon site de recettes - page d'acceuil</title>
+        <link rel="stylesheet" href="Style.css">
     </head>
  
-    <body>
- 
-    <!-- L'en-tête -->
-    
-    <header>
-        <!-- Le menu -->
-    
-        <?php include('header.php'); ?>
-       
-    </header>
-    
-    <!-- Le corps -->
-    
-    <div id="corps">
+    <body>    
+        <?php include('/php/header.php'); ?>
+
         <h1>Mon site de recettes</h1>
-                
-            <p>
-                Bienvenue sur mon site de recettes !
-            </p>
+        
+        <?php 
+            include_once('/php/variables.php');
+            include_once('/php/function.php');
+        ?>
+    
+    <div class="Recipes">
+        <?php
+            foreach(getRecipes($recipes) as $recipe){
+                        foreach($users as $user){
+                            if($recipe['author'] == $user['email']){
+                                echo '<h2>'. $recipe['title']. '</h2><br>';
+                                echo '<p>'.displayAuthor($recipe['author'], $users) . '</p>';
+                            }
+                        }
+            }
+        ?>
         </div>
     
-    <!-- Le pied de page -->
-    
-    <footer id="pied_de_page">
-        <p>Copyright moi, tous droits réservés</p>
-    </footer>
+    <?php include_once('/php/footer.php'); ?>
     
     </body>
 </html>
